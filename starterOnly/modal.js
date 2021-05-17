5969058f7efae13729a7193793e35cf20fb5f044
Lionel Medzo Owono <lionel.medzo-owono@expleogroup.com>
2021-05-17 09:30:10 +0200

    #1 TODO fermer la modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,17 +7,30 @@ function editNav() {
   }
 }
 
+
+
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+const closeFormulaire = document.querySelectorAll(".close");
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+//fermer l'evenement formulaire
+closeFormulaire.forEach((X) => X.addEventListener("click",fermerModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+//fonction pour fermer le formulaire
+function fermerModal(){
+  modalbg.style.display = "none";
 }
 
 
