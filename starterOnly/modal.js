4cd0179a9aa8ff846f7ced6b70665c2dcdcf4177
Lionel Medzo Owono <lionel.medzo-owono@expleogroup.com>
2021-05-21 09:33:02 +0200

    #3 Ajouter validation ou messages d'erreur

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -17,23 +17,34 @@ const formData = document.querySelectorAll(".formData");
 
 const closeFormulaire = document.querySelectorAll(".close");
 
+
 // Liaison des labels
 const prenom = document.getElementById("first");
+const prenom_m = document.getElementById("prenom_erreur");
 const nom =  document.getElementById("last");
+const nom_m =document.getElementById("nom_erreur");
 const email =  document.getElementById("email");
+const email_m = document.getElementById("email_erreur");
 const naissance = document.getElementById("birthdate");
+const naissance_m = document.getElementById("birthdate_erreur");
 const nombreTournois = document.getElementById("quantity");
+const nombreTournois_m = document.getElementById("quantity_erreur");
 const location1 = document.getElementById("location1");
 const location2 =  document.getElementById("location2");
 const location3 =  document.getElementById("location3");
 const location4 =  document.getElementById("location4");
 const location5 =  document.getElementById("location5");
 const location6 =  document.getElementById("location6");
+const location_m = document.getElementById("location_manquant");
 const checkbox1 = document.getElementById("checkbox1");
 const checkbox2 = document.getElementById("checkbox2");
+const condition_m = document.getElementById("condition_erreur");
+;
 
 
-var mailCaractere = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+
+
+var mailCaractere = /^([a-z0-9_\.-]+\@[\da-z\.-]+\.[a-z\.]{2,6})$/;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -47,61 +58,80 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-//fonction pour fermer le formulaire
 function fermerModal(){
   modalbg.style.display = "none";
 }
 
 
 
+
 function validate () {
-  
+
   if (prenom.value === '' || prenom.value.length< 2)
-  
+
   {
-    alert ("Remplir votre prénom avec 2 lettres minimums");  
+    prenom_m.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+    prenom_m.style.fontSize = "14px";
     event.preventDefault();
   }
+  else{
+    prenom_m.textContent = "";
+  }
+  
 
   if (nom.value === '' || nom.value.length < 2)
   {
-    alert ("Remplir votre nom avec 2 lettres minimums");  
+    nom_m.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du nom."; 
+    nom_m.style.fontSize = "14px";
     event.preventDefault();
-  }
-  if (naissance.value ===''){
-    alert("entrez une date de naissance");
-    event.preventDefault();
-  }
- 
-  if (nombreTournois.value === ''){
-    alert ("Indiquez le nombre de tournois GameOn");
-    event.preventDefault();
+  }else{
+    nom_m.textContent = "";
   }
 
-  if(mailCaractere.test(document.getElementById("email").value)){
-    alert(" OK");
+  if (naissance.value ===''){
+    naissance_m.textContent= "Veuillez entrez votre date de naissance";
+    naissance_m.style.fontSize = "14px";
+    event.preventDefault();
   }
   else{
-    alert ("Entrez une adresse mail correct ");  
+    naissance_m.textContent = "";
+  }
+
+  if (nombreTournois.value === ''){
+    nombreTournois_m.textContent = "Veuillez entrer le nombre de tournois participé";
+    nombreTournois_m.style.fontSize = "14px";
+    event.preventDefault();
+  }
+  else{
+    nombreTournois_m.textContent = "";
+  }
+
+  if(mailCaractere.test(email.value)){
+    email_m.textContent = "";
+  }
+  else{
+    email_m.textContent =" Veuillez entrer un email correct";
+    email_m.style.fontSize = "14px";
     event.preventDefault();
   }
 
   if ((location1.checked)|| (location2.checked) || (location3.checked)
     ||(location4.checked) ||(location5.checked) ||(location6.checked)) {
-    alert("bouton selectionné");
+    location_m.textContent = "";
   }
   else{
-    alert ("Selectionner un bouton "); 
+    location_m.textContent = "Vous devez choisir une option";
+    location_m.style.fontSize = "14px"; 
     event.preventDefault();
   }
 
   if(checkbox1.checked){
-    alert("Condition selectionné");
+    condition_m = "";
   }
   else{
-    alert("Veuillez accepter les condtions d'utilisation");
+    condition_m.textContent ="Vous devez vérifier que vous acceptez les termes et conditions";
+    condition_m.style.fontSize = "14px";
     event.preventDefault();
   }
-  
-}
 
+}
